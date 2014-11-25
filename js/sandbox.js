3b5791e2ea1335a509d4ddd0ac175186e1735421
Tiger Huang <tigerrun1998@gmail.com>
2014-11-24 22:27:24 -0500

    Added basic sandbox functionality.

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,1 +1,10 @@
-alert("HI");
+function sandbox_run() {
+	var code = editor.getSession().getValue();
+	eval(code);
+}
+function sandbox_clear() {
+	var output = document.getElementById("sandbox_output");
+	while(output.firstChild) {
+		output.removeChild(output.firstChild);
+	}
+}
